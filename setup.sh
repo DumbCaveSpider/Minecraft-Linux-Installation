06ee8243e16f75681fd5e626f416606a29c0a96c
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2021-09-22 09:19:16 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,66 +1,68 @@
-#!/bin/bash
-
-mkdir bin
-chmod +x *
-export MAIN_DIR=$PWD
-
-# General Checks
+SCRIPT_VERSION="v1.0"
 
 # exit with error status code if user is not root
 if [[ $EUID -ne 0 ]]; then
-  echo "> This script must be executed with root privileges (sudo)." 1>&2
+  echo "* User MUST be under a root user. (sudo -s)." 1>&2
   exit 1
 fi
 
 # check for curl
 if ! [ -x "$(command -v curl)" ]; then
-  echo "> curl is required in order for this script to work."
-  echo "> Install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
   exit 1
 fi
 
-# firewall
-CONFIGURE_FIREWALL=false
-CONFIGURE_UFW=false
-CONFIGURE_FIREWALL_CMD=false
+output() {
+  echo -e "* ${1}"
+}
 
-# intro
-echo "# Minecraft Linux Installation Script by DumbCaveSpider (ArcticWoof#6900) #"
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
 
-# options
-echo "> What would you like to setup?"
-echo "[1] Setup Minecraft Java"
-echo "[2] Setup Minecraft Bedrock"
-read -p "> Please enter a number: " choice
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
 
-# no input
-if [ -z $choice ] then
-  echo "> Invaild Input! Please try again!"
-  
-elif [ $choice == "1" ] then
-  echo "> What type of Java Server would you like to setup?"
-  echo "[1] Vanilla Java (Extra Setup)"
-  echo "[2] PaperMC (Extra Setup)"
-  echo "[3] FabricMC (Extra Setup)"
-  echo "[4] Minecraft Forge (Extra Setup)"
-  echo "[5] Sponge (Extra Setup)"
-  read -p "> Please enter a number: " javaserver
-  
-  if [ $javaserver == "1"] then
+done=false
+
+output "Minecraft Instalation Script | $SCRIPT_VERSION"
+output
+output "Support Server"
+output "https://discord.arcticwoof.ga"
+output
+output "This script is ONLY used for owned Virtual Machine! NOT SHELL MACHINE!"
+
+output
+
+vanilla() {
   bash <(curl -s https://raw.githubusercontent.com/DumbCaveSpider/Minecraft-Linux-Installation/main/JavaInstallScripts/VanillaInstall)
-  
-  elif [ $javaserver == "2"] then
-  bash <(curl -s https://raw.githubusercontent.com/DumbCaveSpider/Minecraft-Linux-Installation/main/JavaInstallScripts/PaperInstall)
-  
-  elif [ $javaserver == "3"] then
-  bash <(curl -s https://raw.githubusercontent.com/DumbCaveSpider/Minecraft-Linux-Installation/main/JavaInstallScripts/FabricInstall)
-  
-  elif [ $javaserver == "4"] then
-  bash <(curl -s https://raw.githubusercontent.com/DumbCaveSpider/Minecraft-Linux-Installation/main/JavaInstallScripts/ForgeInstall)
-  
-  elif [ $javaserver == "5"] then
-  bash <(curl -s https://raw.githubusercontent.com/DumbCaveSpider/Minecraft-Linux-Installation/main/JavaInstallScripts/SpongeInstall)
-  
-else
-  echo "> Invaild Input! Please try again!"
-fi 
+}
+
+paper() {
+  bash <(curl -s https://raw.githubusercontent.com/DumbCaveSpider/Minecraft-Linux-Installation/main/JavaInstallScripts/VanillaInstall)
+}
+
+while [ "$done" == false ]; do
+  options=(
+    "Java"
+    "Bedrock"
+  )
+
+  actions=(
+    "vanilla"
+    "paper"
+  )
+
+  output "Numeber:"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
